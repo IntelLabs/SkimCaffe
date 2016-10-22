9cc22e9c2ae0ffea32633118ebbbe9152d44f97d
Jongsoo Park <jongsoo.park@intel.com>
2016-10-22 11:27:02 -0700

    specify prune threshold with command line parameter

--- models/bvlc_reference_caffenet/train_script.sh
+++ models/bvlc_reference_caffenet/train_script.sh
@@ -8,12 +8,12 @@ model_path="models/bvlc_reference_caffenet"
 
 if [ "$#" -lt 7 ]; then
 	echo "Illegal number of parameters"
-	echo "Usage: train_script base_lr weight_decay kernel_shape_decay breadth_decay block_group_decay device_id template_solver.prototxt [finetuned.caffemodel/.solverstate]"
+	echo "Usage: train_script base_lr weight_decay threshold breadth_decay block_group_decay device_id template_solver.prototxt [finetuned.caffemodel/.solverstate]"
 	exit
 fi
 base_lr=$1
 weight_decay=$2
-kernel_shape_decay=$3
+threshold=$3
 breadth_decay=$4
 block_group_decay=$5
 solver_mode="GPU"
@@ -23,7 +23,7 @@ current_time=$(date)
 current_time=${current_time// /_}
 current_time=${current_time//:/-}
 
-snapshot_path=$folder/${base_lr}_${weight_decay}_${kernel_shape_decay}_${breadth_decay}_${block_group_decay}_${current_time}
+snapshot_path=$folder/${base_lr}_${weight_decay}_${threshold}_${breadth_decay}_${block_group_decay}_${current_time}
 mkdir $snapshot_path
 
 solverfile=$snapshot_path/solver.prototxt
@@ -34,7 +34,8 @@ template_file=$7
 
 cat $folder/${template_file} > $solverfile
 echo "block_group_decay: $block_group_decay" >> $solverfile
-echo "kernel_shape_decay: $kernel_shape_decay" >> $solverfile
+#echo "kernel_shape_decay: $kernel_shape_decay" >> $solverfile
+echo "prune_threshold: $threshold" >> $solverfile
 echo "breadth_decay: $breadth_decay" >> $solverfile
 echo "weight_decay: $weight_decay" >> $solverfile
 echo "base_lr: $base_lr" >> $solverfile
@@ -42,7 +43,7 @@ echo "snapshot_prefix: \"$snapshot_path/$file_prefix\"" >> $solverfile
 #if [ "$#" -ge 6 ]; then
 if [ "$6" -ne "-1" ]; then
 	device_id=$6
-	echo "device_id: $device_id" >> $solverfile
+	#echo "device_id: $device_id" >> $solverfile
 else
 	solver_mode="CPU"
 fi
@@ -55,12 +56,16 @@ if [ "$#" -ge 8 ]; then
 	tunedmodel=$8
 	file_ext=$(echo ${tunedmodel} | rev | cut -d'.' -f 1 | rev)
 	if [ "$file_ext" = "caffemodel" ]; then
-	  ./build/tools/caffe.bin train --solver=$solverfile --weights=$model_path/$tunedmodel  > "${snapshot_path}/train.info" 2>&1
+    if [ "$6" -ne "-1" ]; then
+	    ./build/tools/caffe.bin train -gpu $device_id --solver=$solverfile --weights=$model_path/$tunedmodel  > "${snapshot_path}/train.info" 2>&1
+    else
+	    ../caffe_scnn_cpu_only/build/tools/caffe.bin train --solver=$solverfile --weights=$model_path/$tunedmodel  > "${snapshot_path}/train.info" 2>&1
+    fi
 	else
-	  ./build/tools/caffe.bin train --solver=$solverfile --snapshot=$model_path/$tunedmodel > "${snapshot_path}/train.info" 2>&1
+	  ./build/tools/caffe.bin train -gpu $device_id --solver=$solverfile --snapshot=$model_path/$tunedmodel > "${snapshot_path}/train.info" 2>&1
 	fi
 else
-	./build/tools/caffe.bin train --solver=$solverfile   > "${snapshot_path}/train.info" 2>&1
+	./build/tools/caffe.bin train -gpu $device_id --solver=$solverfile   > "${snapshot_path}/train.info" 2>&1
 fi
 
 cat ${snapshot_path}/train.info | grep loss+ | awk '{print $8 " " $11}' > ${snapshot_path}/loss.info
