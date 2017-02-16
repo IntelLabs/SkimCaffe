4898f6f11d2073dc84efdf6f426bc0adcd57c3fa
Jongsoo Park <jongsoo.park@intel.com>
2017-02-16 14:10:54 -0800

    get rid of warning

--- include/caffe/layer.hpp
+++ include/caffe/layer.hpp
@@ -480,7 +480,9 @@ inline Dtype Layer<Dtype>::Forward(const vector<Blob<Dtype>*>& bottom,
         const int count = top[top_id]->count();
         const Dtype* data = top[top_id]->cpu_data();
         const Dtype* loss_weights = top[top_id]->cpu_diff();
+#ifdef _OPENMP
 #pragma omp parallel for reduction(+:loss)
+#endif
         for (int i = 0; i < count; ++i) {
           loss += data[i]*loss_weights[i];
         }
